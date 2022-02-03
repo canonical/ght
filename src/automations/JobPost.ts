b874ba6951a6869e1879442b822aeacc49dd38f1
Ilayda Cavusoglu <ilayda.cavusoglu@canonical.com>
2022-02-03 16:59:28 +0300

    Updated JobPost

--- src/automations/JobPost.ts
+++ src/automations/JobPost.ts
@@ -1,8 +1,6 @@
-import { rejects } from "assert";
-import { resolve } from "path";
-import Puppeteer from "puppeteer";
+import Puppeteer, { ElementHandle } from "puppeteer";
 import { MAIN_URL } from "../common/constants";
-import { getIDFromURL, getInnerText } from "../common/pageUtils";
+import { getInnerText } from "../common/pageUtils";
 import { Job, Post } from "../common/types";
 
 export default class JobPost {
@@ -13,38 +11,38 @@ export default class JobPost {
         page.setCookie(
             {
                 name: "sessionid",
-                value: "your-session-id",
+                value: "sxqeeh9u4jngesqrgsw5so33r02fmlm8",
                 domain: "login.ubuntu.com"
             }
         );
     }
 
+    private async getIDFromURL(element: ElementHandle, selector: string): Promise<number> {
+        const url = await element.$eval(selector, (anchor: any) => anchor.getAttribute('href'));
+        const urlParts: string[] = ("" + url).split("/");
+        return +urlParts[urlParts.length - 1]; 
+    }
+
     private async getJobPostData() {
         const jobPosts: Post[] = [];
         const posts = await this.page.$$(".job-application");
-        if (!posts || posts.length === 0) {
-            console.error("No post found!");
-            return jobPosts;
-        }
-    
+        if (!posts || posts.length === 0)
+            throw new Error("No post found!");
+                
         for (let post of posts) {
             const postTitle = await post.$(".job-application__name"); 
-            if (!postTitle) {
-                console.error("Post title cannot be found");
-                continue;
-            }
+            if (!postTitle) throw new Error("Post title cannot be found");
+
             const innerText = await getInnerText(postTitle);
             const titleLocationInfo = innerText.split("\n").map((e: string) => e.trim()).filter((e: string) => !!e);
     
-            const jobPostID = await getIDFromURL(postTitle, "a[href*='https://boards.greenhouse.io/']");
+            const jobPostID = await this.getIDFromURL(postTitle, "a[href*='https://boards.greenhouse.io/']");
             
             const postBoard = await post.$(".board-column"); 
-            if (!postBoard) {
-                console.error("Post board cannot be found");
-                continue;
-            }
+            if (!postBoard) throw new Error("Post board cannot be found");
+
             const boardName = await getInnerText(postBoard);
-            const boardID = await getIDFromURL(postBoard, "a");
+            const boardID = await this.getIDFromURL(postBoard, "a");
     
             jobPosts.push({
                 id: jobPostID,
@@ -59,12 +57,12 @@ export default class JobPost {
         return jobPosts;
     }
     
-    private async getJobName(): Promise<string | null> {
+    private async getJobName(): Promise<string> {
         const jobTitleElement = await this.page.$(".job-name");
         const jobAnchor = await jobTitleElement?.$("a");
-
-        return jobAnchor ? await getInnerText(jobAnchor) : 
-            new Promise(reject => reject(null));
+        
+        if(!jobAnchor) throw new Error("job name not found");
+        return await getInnerText(jobAnchor);
     }
     
     public async getJobData(jobIDs: number[]): Promise<Job[]> {
@@ -74,27 +72,16 @@ export default class JobPost {
             await this.page.goto(jobappURL);
             
             const jobTitle = await this.getJobName(); 
-            if (!jobTitle) {
-                console.error("Job title cannot be found.");
-                continue;
-            }
     
             const pageElements = await this.page.$$("*[aria-label*=Page]");
-            let jobPostInfo: Post[] = [];
-            if (pageElements) {
-                // check if there is more than 1 page
-                const pageCount = pageElements.length; 
-                if (pageCount > 0) {
-                    for (let currentPage = 1; currentPage <= pageCount; currentPage++) {
-                        await this.page.goto(`${jobappURL}?page=${currentPage}`);
-                        jobPostInfo.push(...await this.getJobPostData());
-                    }
-                } else {
-                    jobPostInfo = await this.getJobPostData();
-                }
-            } else {
-                console.error("Page information cannot be found!");
+            if (!pageElements) throw new Error("Page information cannot be found");
+            
+            let jobPostInfo: Post[] = []; 
+            for (let currentPage = 1; currentPage <= pageElements.length; currentPage++) {
+                await this.page.goto(`${jobappURL}?page=${currentPage}`);
+                jobPostInfo.push(...await this.getJobPostData());
             }
+
             jobData.push({
                 name: jobTitle,
                 id: jobID,
--- src/common/pageUtils.ts
+++ src/common/pageUtils.ts
@@ -1,10 +1,4 @@
-import Puppeteer, { ElementHandle } from "puppeteer";
-
-export async function getIDFromURL(element: ElementHandle, selector: string): Promise<number> {
-    const url = await element.$eval(selector, (anchor: any) => anchor.getAttribute('href'));
-    const urlParts: string[] = ("" + url).split("/");
-    return +urlParts[urlParts.length - 1]; 
-}
+import Puppeteer from "puppeteer";
 
 export function getInnerText(element: Puppeteer.ElementHandle): Promise<string> {
     return element?.evaluate((el: Element) => (el as HTMLElement).innerText);
