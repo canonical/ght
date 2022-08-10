e5a5020ce0a81939e12b5c5c29b2eb4a36c4d237
andesol <anddessol@gmail.com>
2022-08-10 16:03:50 +0200

    refactor loop

--- src/assign-graders/LoadBalancer.ts
+++ src/assign-graders/LoadBalancer.ts
@@ -14,6 +14,7 @@ export default class LoadBalancer {
     private graders: Grader[];
     private jobs: string[];
     private spinner: Ora;
+    private currentUser: string = "";
 
     constructor(
         page: Page,
@@ -45,7 +46,7 @@ export default class LoadBalancer {
                     .trim();
                 const candidate = p.querySelector(".name a")?.textContent;
 
-                if (applicationID && candidate && job && toGrade) {
+                if (applicationID && candidate && job && toGrade != null) {
                     return {
                         applicationID,
                         candidate,
@@ -55,35 +56,6 @@ export default class LoadBalancer {
                 }
             })
         );
-    }
-
-    /**
-     * Get applications to process one at a time
-     */
-    private async *getApplicationsToProcess() {
-        while (true) {
-            await this.page.waitForSelector(".person");
-            const applicationsPage = await this.getApplicationsPage();
-
-            for (const application of applicationsPage) {
-                if (
-                    application &&
-                    application?.toGrade &&
-                    this.jobs.includes(application.job)
-                ) {
-                    yield application;
-                }
-            }
-
-            // Keep doing this until there are no more pages
-            const nextPageBtn = await this.page.$("a.next_page:not(.disabled)");
-            if (!nextPageBtn) return;
-
-            await Promise.all([
-                this.page.waitForNavigation(),
-                nextPageBtn.click(),
-            ]);
-        }
     }
 
     /**
@@ -124,9 +96,87 @@ export default class LoadBalancer {
      * Find current user name in UI
      */
     private async findUsername() {
-        return await this.page.$eval(
+        const currentUser = await this.page.$eval(
             "script[data-user-name]",
             (el) => (el as HTMLElement).dataset.userName
+        );
+        if (!currentUser) {
+            throw new Error("Unable to find user's name in Greenhouse");
+        }
+        this.currentUser = currentUser;
+    }
+
+    /**
+     * Assign graders to one application
+     */
+    private async assignGradersToApplication(application: Application) {
+        this.spinner.start("Processing applications");
+
+        const selector = `.person[application="${application?.applicationID}"]`;
+
+        // Click toggle button
+        await this.page.waitForSelector(`${selector} .toggle-interviews`);
+        await this.page.$eval(`${selector} .toggle-interviews`, (toggle) =>
+            (toggle as HTMLAnchorElement).click()
+        );
+
+        // Click edit
+        await this.page.waitForSelector(
+            `${selector} .edit-take-home-test-graders-link`
+        );
+
+        await this.page.$eval(
+            `${selector} .edit-take-home-test-graders-link`,
+            (btn) => (btn as HTMLAnchorElement).click()
+        );
+
+        // Wait for modal to open
+        await this.page.waitForSelector("ul.chzn-choices", {
+            visible: true,
+        });
+
+        // Read graders already assigned
+        const gradersAssigned = await this.page.$$eval(
+            "ul .search-choice span",
+            (el) => el.map((grader) => grader.textContent)
+        );
+        // Skip if already graders assigned
+        if (gradersAssigned.length >= 2) {
+            return;
+        }
+        // Skip if only one grader but it's not the user
+        if (
+            gradersAssigned.length === 1 &&
+            gradersAssigned[0] !== this.currentUser
+        ) {
+            return;
+        }
+
+        // Click input field
+        await this.page.waitForSelector(".search-field input[type='text']");
+        await this.page.click(".search-field input[type='text']");
+
+        // If there's only one grader and is the user running the command remove it
+        // (hiring leads are assigned by default as graders)
+        if (
+            gradersAssigned.length === 1 &&
+            gradersAssigned[0] === this.currentUser
+        ) {
+            await this.page.keyboard.press("Backspace");
+            await this.page.keyboard.press("Backspace");
+        }
+
+        const [grader1, grader2] = this.findRandomGraders(application);
+        await this.writeGrader(grader1);
+        await this.writeGrader(grader2);
+
+        // Click save
+        await this.page.click("input[type='submit']");
+
+        this.spinner.stop();
+        console.log(
+            green("✔"),
+            `Written Interview from ${application.candidate} assigned to: ${grader1.name}, ${grader2.name}`
         );
     }
 
@@ -134,82 +184,29 @@ export default class LoadBalancer {
         await this.page.goto(
             `${MAIN_URL}people?sort_by=last_activity&sort_order=desc&stage_status_id%5B%5D=2&in_stages%5B%5D=Written+Interview`
         );
+        await this.findUsername();
 
-        const currentUser = await this.findUsername();
-        if (!currentUser) {
-            throw new Error("Unable to find user's name in Greenhouse");
+        while (true) {
+            await this.page.waitForSelector(".person");
+            const applicationsPage = await this.getApplicationsPage();
+            for (const application of applicationsPage) {
+                if (
+                    application &&
+                    application?.toGrade &&
+                    this.jobs.includes(application.job)
+                ) {
+                    await this.assignGradersToApplication(application);
+                }
+            }
+
+            // Keep doing this until there are no more pages
+            const nextPageBtn = await this.page.$("a.next_page:not(.disabled)");
+            if (!nextPageBtn) break;
+
+            await Promise.all([
+                this.page.waitForNavigation(),
+                nextPageBtn.click(),
+            ]);
         }
-
-        this.spinner.start("Processing applications\n");
-
-        for await (const application of this.getApplicationsToProcess()) {
-            const selector = `.person[application="${application?.applicationID}"]`;
-
-            // Click toggle button
-            await this.page.waitForSelector(`${selector} .toggle-interviews`);
-            await this.page.$eval(`${selector} .toggle-interviews`, (toggle) =>
-                (toggle as HTMLAnchorElement).click()
-            );
-
-            // Click edit
-            await this.page.waitForSelector(
-                `${selector} .edit-take-home-test-graders-link`
-            );
-
-            await this.page.$eval(
-                `${selector} .edit-take-home-test-graders-link`,
-                (btn) => (btn as HTMLAnchorElement).click()
-            );
-
-            // Wait for modal to open
-            await this.page.waitForSelector("ul.chzn-choices", {
-                visible: true,
-            });
-
-            // Read graders already assigned
-            const gradersAssigned = await this.page.$$eval(
-                "ul .search-choice span",
-                (el) => el.map((grader) => grader.textContent)
-            );
-            // Skip if already graders assigned
-            if (gradersAssigned.length >= 2) {
-                continue;
-            }
-            // Skip if only one grader but it's not the user
-            if (
-                gradersAssigned.length === 1 &&
-                gradersAssigned[0] !== currentUser
-            ) {
-                continue;
-            }
-
-            // Click input field
-            await this.page.waitForSelector(".search-field input[type='text']");
-            await this.page.click(".search-field input[type='text']");
-
-            // If there's only one grader and is the user running the command remove it
-            // (hiring leads are assigned by default as graders)
-            if (
-                gradersAssigned.length === 1 &&
-                gradersAssigned[0] === currentUser
-            ) {
-                await this.page.keyboard.press("Backspace");
-                await this.page.keyboard.press("Backspace");
-            }
-
-            const [grader1, grader2] = this.findRandomGraders(application);
-            await this.writeGrader(grader1);
-            await this.writeGrader(grader2);
-
-            // Click save
-            await this.page.click("input[type='submit']");
-
-            console.log(
-                green("✔"),
-                `Written Interview from ${application.candidate} assigned to: ${grader1.name}, ${grader2.name}`
-            );
-        }
-
-        this.spinner.stop();
     }
 }
