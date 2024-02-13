d5b9a177baf3eed262151acefe0328e2eff534ce
Andreu <20623084+andesol@users.noreply.github.com>
2024-02-13 16:18:05 +0100

    fix build after updating dependencies (#225)

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -59,7 +59,7 @@ export abstract class BaseController {
             };
         } else {
             options = {
-                headless: true,
+                headless: "new",
                 defaultViewport: null,
                 args: ["--no-sandbox"],
             };
